abeebbd5cb2961cf547aa6d4a4c11e108f9052be
YourAKShaw <hitme@yourakshaw.com>
2024-08-01 15:28:22 +0530

    feat(user): :zap: add more routes for User

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { register, login } from "../controllers/userController";
+import {
+  register,
+  login,
+  getUserProfile,
+  updateUserProfile,
+  updateUserPassword,
+} from "../controllers/userController";
+import { authenticate } from "../middlewares/authenticate";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", authenticate, getUserProfile);
+router.put("/profile", authenticate, updateUserProfile);
+router.put("/profile/password", authenticate, updateUserPassword);
 
 export default router;
