e3b3300df0f2e64595771320cc5c5b3b45e6ffed
YourAKShaw <hitme@yourakshaw.com>
2024-08-01 12:17:45 +0530

    docs(swagger): :memo: integrate swagger into app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import productRoutes from "./routes/productRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import userRoutes from "./routes/userRoutes";
+import { setupSwagger } from "./utils/swagger";
 
 const app = express();
+
+setupSwagger(app);
 
 app.use(bodyParser.json());
 app.use("/products", productRoutes);
